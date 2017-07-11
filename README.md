d9b343dfb100a12a18162fa47f07a4123f320336
Romulo Mourão <romulo.dam@gmail.com>
2017-07-11 19:37:39 -0300

    beta

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tracemap
+
+![](2017-07-11-19-27-44.png)
+
+##### Tracemap é um aplicativo para visualizar a saída do comando `traceroute`. Para fins educacionais apenas
+*Tracemap is an app to visualize output of traceroute on the map. For educational purpose only.*
+
+>Este projeto foi desenvolvido por Romulo Mourão
+>para a disciplina de Redes II com o professor Célio Albuquerque 
+> na Universidade Federal Fluminense em 2017.1
+
+
+`This project was developed by Romulo Mourão`
+
--- renderer.js
+++ renderer.js
@@ -11,25 +11,6 @@ var time;
 let ipArray;
 let domain;
 var s;
-let graph = {};
-
-var Datastore = require('nedb');
-var db = new Datastore({
-    filename: 'nodes',
-    autoload: true
-});
-
-var dbe = new Datastore({
-    filename: 'edges',
-    autoload: true
-});
-
-
-
-$("#show-graph").click(function () {
-    showGraph();
-
-});
 
 
 
@@ -73,7 +54,6 @@ window.onkeypress = function (e) {
                     showSearchedDomain(domain);
                     appendInfos(resultados);
                     setPath();
-                    saveEdges();
 
                 })
                 .catch(erro => console.error(erro));
@@ -111,81 +91,14 @@ function appendInfos(resultados) {
         console.log(data.status);
         if (data.status === "success") {
             Api.appendResponse(data, index, time);
-            saveNodes(data);
             ipArray.push(data.query);
 
         }
     });
 }
 
-function saveNodes(data) {
-    db.findOne({
-        ip: data.query,
-    }, function (err, node) {
-        if (!node) {
-            console.log("inserir", data.query);
-            db.insert({
-                id: data.query,
-                label: data.query,
-                size: 1,
-                x: data.lat + (2 * Math.random()),
-                y: data.lon + (2 * Math.random()),
-                color: "#666"
-            });
-        } else {
-            console.log("repetido", data.query);
-            let relevance = node.size + 1;
-            db.update({
-                ip: data.query
-            }, {
-                $set: {
-                    size: relevance
-                }
-            }, {}, function () {});
-        }
-    });
-}
-
-function saveEdges() {
-    for (let i = 0; i < ipArray.length - 1; i++) {
-        dbe.insert({
-            id: ipArray[i],
-            label: domain,
-            source: ipArray[i],
-            target: ipArray[i + 1],
-            size: 1,
-            color: "#00bc64",
-            type: "curvedArrow"
-        });
-    }
-}
-
-function showGraph() {
-    prepareGraph();
-
-    $("#map").prop('id', 'graph');
-
-    s = new sigma({
-        graph: graph,
-        renderer: {
-            container: document.getElementById('graph'),
-            type: 'canvas'
-        },
-        settings: {
-            edgeLabelSize: 'proportional'
-        }
-    });
 
 
-}
 
-function prepareGraph() {
-    db.find({}, function (err, nodes) {
-        graph.nodes = nodes;
-    });
 
-    dbe.find({}, function (err, edges) {
-        graph.edges = edges;
-    });
-}
 
